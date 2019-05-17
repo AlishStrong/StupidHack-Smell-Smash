849e2dc251e773b9237b0f8e8bc22e0bf8fe51cc
unknown <mannghi.tranvn@gmail.com>
2019-05-17 22:25:36 +0300

    working button

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      isOn: false,
+      start: 0,
+      stop: false
+    };
+  }
+  startFart = async () => {
+    setTimeout(() => {
+      Alert.alert("timeout!");
+      this.startFart();
+    }, 10000);
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={() => this.startFart()} title="press me" />
       </View>
     );
   }
@@ -14,8 +29,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
